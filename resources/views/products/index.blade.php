eae2c52d6a688efb176546147df50e30a1428dcf
carlosrivanunes <106713646+carlosrivanunes@users.noreply.github.com>
2025-11-28 00:30:17 -0300

    Include Bootstrap JS in products index view
    Added Bootstrap JavaScript bundle for enhanced functionality.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="..." crossorigin="anonymous"></script>
+
+
 <style>
     /* Variáveis de Estilo da Página Inicial (Padronização) */
     :root {
